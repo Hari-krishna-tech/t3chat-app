4e34d3acb8128f8994e57086ad67ca0c89ae8494
Hari-krishna-tech <jvenkadam@gmail.com>
2025-06-14 22:48:02 +0530

    changed seed.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,10 @@
-import prisma from '../lib/prisma'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 async function main() {
   const response = await Promise.all([
-    prisma.users.upsert({
+    prisma.user.upsert({
       where: { email: 'rauchg@vercel.com' },
       update: {},
       create: {
@@ -12,7 +14,7 @@ async function main() {
           'https://images.ctfassets.net/e5382hct74si/2P1iOve0LZJRZWUzfXpi9r/9d4d27765764fb1ad7379d7cbe5f1043/ucxb4lHy_400x400.jpg',
       },
     }),
-    prisma.users.upsert({
+    prisma.user.upsert({
       where: { email: 'lee@vercel.com' },
       update: {},
       create: {
@@ -22,7 +24,7 @@ async function main() {
           'https://images.ctfassets.net/e5382hct74si/4BtM41PDNrx4z1ml643tdc/7aa88bdde8b5b7809174ea5b764c80fa/adWRdqQ6_400x400.jpg',
       },
     }),
-    prisma.users.upsert({
+    prisma.user.upsert({
       where: { email: 'harikrishna@vercel.com' },
       update: {},
       create: {
@@ -32,7 +34,7 @@ async function main() {
           'https://images.ctfassets.net/e5382hct74si/4BtM41PDNrx4z1ml643tdc/7aa88bdde8b5b7809174ea5b764c80fa/adWRdqQ6_400x400.jpg',
       },
     }),
-    await prisma.users.upsert({
+    prisma.user.upsert({
       where: { email: 'stey@vercel.com' },
       update: {},
       create: {
@@ -45,6 +47,7 @@ async function main() {
   ])
   console.log(response)
 }
+
 main()
   .then(async () => {
     await prisma.$disconnect()
