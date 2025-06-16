d335f335f6868b807693e90edb5f52c8027bac86
Hari-krishna-tech <jvenkadam@gmail.com>
2025-06-16 10:58:12 +0530

    change signin

--- app/auth/signin/SignInButton.tsx
+++ app/auth/signin/SignInButton.tsx
@@ -5,7 +5,7 @@ import { signIn } from "next-auth/react";
 export default function SignInButton({ provider }: { provider: any }) {
   return (
     <button
-      onClick={() => signIn(provider.id)}
+      onClick={() => signIn(provider.id, { callbackUrl: '/' })}
       className="w-full rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
     >
       Sign in with {provider.name}
